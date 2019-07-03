264724e214f5fda8aa1c661daf691d2a6792318f
Oskar Thoren <ot@oskarthoren.com>
2019-07-03 12:03:30 +0800

    Piggyback on Whisper's payload for now

--- eth/p2p/rlpx_protocols/pss_protocol.nim
+++ eth/p2p/rlpx_protocols/pss_protocol.nim
@@ -2,90 +2,102 @@ import
   algorithm, bitops, endians, math, options, sequtils, strutils, tables, times,
   secp256k1, chronicles, chronos, eth/common/eth_types, eth/[keys, rlp, async_utils],
   hashes, byteutils, nimcrypto/[bcmode, hash, keccak, rijndael, sysrand],
-  eth/p2p
-#  eth/p2p/rlpx_protocol/whisper_protocol as whisper
+  eth/p2p, whisper_protocol as whisper
 
-echo "hello pss"
+# Piggybacking on Whisper payload for now
+type Payload* = whisper.Payload
+type DecodedPayload* = whisper.DecodedPayload
 
-#cool
+# Piggybacking on Whisper encode payload method for now
+proc encode*(payload: Payload): Option[Bytes] =
+  return whisper.encode(payload)
 
-# Core concepts
-# 1. Encryption key - type?
-# 2. Topic - type?
-# 3. Message payload
-
-type
-  Hash* = MDigest[256]
-  SymKey* = array[256 div 8, byte] ## AES256 key, 8 bits
-  Topic* = array[4, byte]
-
-  Payload* = object
-    ## XXX: Taken from Whisper, how is this different for PSS?
-    ## Payload is what goes in the data field of the Envelope
-
-    src*: Option[PrivateKey] ## Optional key used for signing message
-    dst*: Option[PublicKey] ## Optional key used for asymmetric encryption
-    symKey*: Option[SymKey] ## Optional key used for symmetric encryption
-    payload*: Bytes ## Application data / message contents
-    padding*: Option[Bytes] ## Padding - if unset, will automatically pad up to
-                            ## nearest maxPadLen-byte boundary
-
-  DecodedPayload* = object
-    src*: Option[PublicKey] ## If the message was signed, this is the public key
-                            ## of the source
-    payload*: Bytes ## Application data / message contents
-    padding*: Option[Bytes] ## Message padding
-
-  Envelope* = object
-    ## Taken from whisper_protocol.nim, in types.go these are the same
-    ## What is on the wire
-    ## XXX: Do we actually want all these fields?
-    expiry*: uint32 ## Unix timestamp when message expires
-    ttl*: uint32 ## Time-to-live, seconds - message was created at (expiry - ttl)
-    topic*: Topic
-    data*: Bytes ## Payload, as given by user
-    nonce*: uint64 ## Nonce used for proof-of-work calculation
-
-  # Corresponds to types.go PssMsg
-  PssMsg* = object
-    dst*: array[4, byte] # Size?
-    control*: array[4, byte] # What is this? Also size
-    expire*: uint32
-    payload*: Envelope # whisper.Envelope (?) / Bytes
-
-proc encode*(self: Payload): Option[Bytes] =
-  ## XXX: Mocking encode method
-  ## Encode a payload according so as to make it suitable to put in an Envelope
-  echo "encode payload len ", self.payload.len
-
-  # assume base64
-
-  # Base64, just for testing
-  let naiveEncode = self.payload.encode()
-
-  #  totalLen = dataLen + padLen
-  let totalLen = len(naiveEncode)
-  var plain = newSeqOfCap[byte](totalLen)
-  plain.add(naiveEncode)
-
-  return some(plain)
-
-proc decode*(data: openarray[byte]): Option[DecodedPayload] =
-  ## Decode data into payload, potentially decrypting
-  ## Should assume malformated data
-  var res: DecodedPayload
-  var plain: Bytes
-
-  echo " decode 1"
-  # Wtf
-#  echo "  decoded", data.decode()
-
-  echo " decode 2"
-  plain = @data
-  echo " decode 3"
-
-  return some(res)
+# Piggybacking on Whisper decode payload method for now
+proc decode*(data: openarray[byte], dst = none[PrivateKey](),
+             symKey = none[SymKey]()): Option[DecodedPayload] =
+  return whisper.decode(data, dst, symKey)
 
 
 
-# Look into connectivity later
+# #cool
+
+# # Core concepts
+# # 1. Encryption key - type?
+# # 2. Topic - type?
+# # 3. Message payload
+
+# type
+#   Hash* = MDigest[256]
+#   SymKey* = array[256 div 8, byte] ## AES256 key, 8 bits
+#   Topic* = array[4, byte]
+
+#   Payload* = object
+#     ## XXX: Taken from Whisper, how is this different for PSS?
+#     ## Payload is what goes in the data field of the Envelope
+
+#     src*: Option[PrivateKey] ## Optional key used for signing message
+#     dst*: Option[PublicKey] ## Optional key used for asymmetric encryption
+#     symKey*: Option[SymKey] ## Optional key used for symmetric encryption
+#     payload*: Bytes ## Application data / message contents
+#     padding*: Option[Bytes] ## Padding - if unset, will automatically pad up to
+#                             ## nearest maxPadLen-byte boundary
+
+#   DecodedPayload* = object
+#     src*: Option[PublicKey] ## If the message was signed, this is the public key
+#                             ## of the source
+#     payload*: Bytes ## Application data / message contents
+#     padding*: Option[Bytes] ## Message padding
+
+#   Envelope* = object
+#     ## Taken from whisper_protocol.nim, in types.go these are the same
+#     ## What is on the wire
+#     ## XXX: Do we actually want all these fields?
+#     expiry*: uint32 ## Unix timestamp when message expires
+#     ttl*: uint32 ## Time-to-live, seconds - message was created at (expiry - ttl)
+#     topic*: Topic
+#     data*: Bytes ## Payload, as given by user
+#     nonce*: uint64 ## Nonce used for proof-of-work calculation
+
+#   # Corresponds to types.go PssMsg
+#   PssMsg* = object
+#     dst*: array[4, byte] # Size?
+#     control*: array[4, byte] # What is this? Also size
+#     expire*: uint32
+#     payload*: Envelope # whisper.Envelope (?) / Bytes
+
+# proc encode*(self: Payload): Option[Bytes] =
+#   ## XXX: Mocking encode method
+#   ## Encode a payload according so as to make it suitable to put in an Envelope
+#   echo "encode payload len ", self.payload.len
+
+#   # assume base64
+
+#   # Base64, just for testing
+#   let naiveEncode = self.payload.encode()
+
+#   #  totalLen = dataLen + padLen
+#   let totalLen = len(naiveEncode)
+#   var plain = newSeqOfCap[byte](totalLen)
+#   plain.add(naiveEncode)
+
+#   return some(plain)
+
+# proc decode*(data: openarray[byte]): Option[DecodedPayload] =
+#   ## Decode data into payload, potentially decrypting
+#   ## Should assume malformated data
+#   var res: DecodedPayload
+#   var plain: Bytes
+
+#   echo " decode 1"
+#   # Wtf
+# #  echo "  decoded", data.decode()
+
+#   echo " decode 2"
+#   plain = @data
+#   echo " decode 3"
+
+#   return some(res)
+
+
+
+# # Look into connectivity later
--- tests/p2p/test_pss.nim
+++ tests/p2p/test_pss.nim
@@ -1,6 +1,3 @@
-# import options, unittest,
-#     eth/p2p/rlpx_protocols/pss_protocol as pss
-
 import
     sequtils, options, unittest, times, tables,
     nimcrypto/hash,
@@ -9,23 +6,15 @@ import
 
 suite "PSS payload":
   test "should roundtrip without keys":
+    # Piggyback on Whisper's payload for now
 
-    echo "1"
     let payload = Payload(payload: @[byte 0, 1, 2])
-    echo "2"
-    let encoded = pss.encode(payload)
+    let encoded = encode(payload)
 
-    echo "test encoded ", encoded
-
-    # Fails here
-    echo "get ", encoded.get()
     let decoded = pss.decode(encoded.get())
-
-    echo "test decoded ", decoded
     check:
-        1 == 1
-    #   decoded.isSome()
-    #   payload.payload == decoded.get().payload
-    #   decoded.get().src.isNone()
-    #   decoded.get().padding.get().len == 251 # 256 -1 -1 -3
-
+      1 == 1
+      decoded.isSome()
+      payload.payload == decoded.get().payload
+      decoded.get().src.isNone()
+      decoded.get().padding.get().len == 251 # 256 -1 -1 -3
