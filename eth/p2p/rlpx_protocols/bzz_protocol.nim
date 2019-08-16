9562931d3b30e9b9ded3f6884d5f5855e7336ff5
nolash <dev@holbrook.no>
2019-08-16 23:41:40 +0200

    Built handshake type (but fails golang peer)
    attempted connect with golang swarm node built from
    ethersphere/swarm commit c535b271536d0dee5bd97c2541ca32a42f272d4f
    
    error:
    Handshake error: Invalid message (RLP error): <= msg #0 (199 bytes): rlp: expected List

--- eth/p2p/rlpx_protocols/bzz_protocol.nim
+++ eth/p2p/rlpx_protocols/bzz_protocol.nim
@@ -2,17 +2,35 @@ import
   eth/p2p, chronos, chronicles, eth/rlp, chronos/timer
 
 const
-  bzzVersion = 42
+  bzzVersion = 12
 
-p2pProtocol Bzz(version = bzzVersion):
-
-  proc hellooo(peer: Peer,
-    foo: string) =
-     echo foo
-    
+p2pProtocol Bzz(version = bzzVersion,
+                rlpxName = "bzz"):
+ 
   onPeerConnected do (peer: Peer):
     warn "conn"
-    if peer.supports(Bzz):
-      warn "bzz"
-      waitFor peer.hellooo("bar")
-      waitFor sleepAsync(1000)
+    var
+      oad = newSeq[byte](32)
+      en: string = "enode://040123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef@127.0.0.1:30399" #newSeq[byte](32) 
+      uad = newSeq[byte](len(en))
+      cps: array[0..0, array[0..1, byte]]
+      cp: array[0..1, byte] # light node legacy preset capability 
+    cp[0] = byte(0) # zeroed by default?
+    cp[1] = byte(3)
+    cps[0] = cp
+    for i in byte(0)..byte(31):
+      oad[i] = i
+    var i: int = 0
+    for e in en:
+      uad[i] = byte(ord(e))
+      i.inc()
+    waitFor peer.hs(12, 4, array([oad, uad]), cps)
+    waitFor sleepAsync(1000)
+
+  proc hs(peer: Peer,
+    version: uint,
+    networkid: uint,
+    ad: array[0..1, seq[byte]],
+    cp: array[0..0, array[0..1, byte]]) =
+    echo rlp.encode(ad)
+    
--- tests/p2p/test_bzz_protocol.nim
+++ tests/p2p/test_bzz_protocol.nim
@@ -1,15 +1,27 @@
 import
-  sequtils, options, unittest, tables, chronos, eth/[keys, p2p],
-  eth/p2p/rlpx_protocols/whisper_protocol, eth/p2p/peer_pool, eth/p2p/rlpx_protocols/bzz_protocol, ./p2p_test_helper
+  sequtils, options, unittest, tables, chronos, eth/[keys, p2p], chronos, 
+  eth/p2p/rlpx_protocols/whisper_protocol, eth/p2p/[enode, peer_pool], eth/p2p/rlpx_protocols/bzz_protocol, ./p2p_test_helper
 
-suite "Whisper connections":
-  var node1 = setupTestNode(Bzz)
-  var node2 = setupTestNode(Bzz)
-  node2.startListening()
-  waitFor node1.peerPool.connectToNode(newNode(initENode(node2.keys.pubKey,
-                                                         node2.address)))
+var nextPort = 30303
+
+proc setupBzzTestNode*(capabilities: varargs[ProtocolInfo, `protocolInfo`],
+  networkId: uint): EthereumNode =
+  let keys1 = newKeyPair()
+  result = newEthereumNode(keys1, localAddress(nextPort), networkId, nil, "bzz-nim/0.0.2",
+                           addAllCapabilities = false)
+  nextPort.inc
+  for capability in capabilities:
+    result.addCapability capability
+
+suite "Bzz connections":
+  var node1 = setupBzzTestNode(Bzz, 4)
+  var node2Pub = keys.initPublicKey("0x818d337ee5a93dc699850463f6cc73c52d0d2d7cd3b1d58949760dba801e873ae9c57611d32dcf2f82cc14fc4619ea1124e4d59641f8e47d40ab2115f6893b06")
+  var node2Port = Port(30399)
+  var node2Addr = Address(udpPort: node2Port, tcpPort: node2Port, ip: parseIpAddress("127.0.0.1"))
+
+  waitFor node1.peerPool.connectToNode(newNode(initENode(node2Pub,
+    node2Addr)))
   asyncTest "Two peers connected":
+    waitFor sleepAsync(2000)
     check:
       node1.peerPool.connectedNodes.len() == 1
-
-
